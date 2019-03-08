75f9c1cfec8b316d2a99fb1c5a182d885d1e76de
Emily Brumfield <belac54@hotmail.com>
2019-03-08 16:23:05 -0500

    implement basic methods

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-let cards = [
+let deck = [
     {title: "Orcs!", body: "1d6 orcs appear!", color: "red"},
     {title: "Treasure!", body: "You find a potion of healing.", color: "green"}
 ]
 
 let data = {
-    cards: cards,
+  deck: deck,  
+  cards: [].concat(deck),
+  drawn: [],
 }
 
 let app = new Vue({
     el: "#app",
-    data: data
-  })
+    data: data,
+    methods: {
+      resetDeck: function () {
+        this.cards = [].concat(this.deck);
+      },
+      drawCard: function () {
+        this.drawn = this.drawn.concat(this.cards[0]);
+        this.cards = this.cards.slice(1);
+      }
+    }
+})
+
+/*
+reset deck function: use cards [].concat(deck)
+drawing cards: move to drawn array; just show the latest drawn array entry and nothing else, at least for now
+
+*/
