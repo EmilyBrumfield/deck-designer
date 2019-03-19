816eba10d6edbb87f66d207e8eee1d7d9b188d90
Emily Brumfield <belac54@hotmail.com>
2019-03-19 01:09:22 -0400

    made cards bigger

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//deck should eventually combine different arrays for different color values or titles or whatever
-//so that only the contents need to be different each time, saving work
-
-//creates a monster card
 function Monster(body, title, color) {
 
   this.body = body;
@@ -62,35 +58,28 @@ function Treasure(body, title, color) {
   }
 }
 
-let deck = [
-  new Monster("8 bugbears approach."),
+
+let sampleDeck = [
+  new Monster("1d8 bugbears approach."),
   new Treasure("You find a potion of healing."),
   new Event("The weather gets worse."),
   new Event("The weather gets better."),
   {title: "Quest", body: "You discover a map to the Temple of Skulls, along with a key to its secret entrance.", color: "orange"}
 ]
 
-/*
-let deck = [
-  new Monster("Orcs!", "Bob"),
-  new Monster("Bugbears!"),
-  new Monster(),
-  new Monster("Ogres!"),
-  new Monster("Ninjas!"),
-  new Treasure("You find a potion of healing."),
-  new Event("The weather gets worse."),
-  new Event("The weather gets better."),
-  {title: "Unique", body: "This card is unique.", color: "purple"}
+let cthulhuDeck = [
+  new Monster("Cultists"),
+  new Monster("Deep Ones"),
+  new Monster("Ghouls"),
+  new Monster("Mi-go"),
+  new Monster("Shoggoth"),
+  new Monster("Chthonian"),
+  new Monster("Starspawn of Cthulhu"),
+  new Monster("Dark Young"),
+  new Monster("Gugs")
 ]
-*/
 
-/*
-let deck = [
-    {title: "Orcs!", body: "1d6 orcs appear!", color: "red"},
-    {title: "Treasure!", body: "You find a potion of healing.", color: "green"},
-    {title: "Event!", body: "The weather gets worse.", color: "blue"}
-]
-*/
+let deck = sampleDeck;
 
 let data = {
   deck: deck,  
@@ -116,9 +105,3 @@ let app = new Vue({
       }
     }
 })
-
-/*
-reset deck function: use cards [].concat(deck)
-drawing cards: move to drawn array; just show the latest drawn array entry and nothing else, at least for now
-
-*/
