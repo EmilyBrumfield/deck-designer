c7c58ce018be4e29610da12ab1aa4edc72f354f2
Emily Brumfield <belac54@hotmail.com>
2019-03-25 06:22:10 -0400

    prevent multiple file drops

--- index.js
+++ index.js
@@ -31,19 +31,27 @@ function dropHandler(ev) {
   // Prevent default behavior (Prevent file from being opened)
   ev.preventDefault();
 
-  if (ev.dataTransfer.items) {
+  if (ev.dataTransfer.items) {    
     // Use DataTransferItemList interface to access the file(s)
-    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-      // If dropped items aren't files, reject them
-      if (ev.dataTransfer.items[i].kind === 'file') {
-        var file = ev.dataTransfer.items[i].getAsFile();
-        console.log('... file[' + i + '].name = ' + file.name);
+    if (ev.dataTransfer.items.length > 1) {
+      alert("Cannot load multiple files.")
+    } else {
+      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+        // If dropped items aren't files, reject them
+        if (ev.dataTransfer.items[i].kind === 'file') {
+          var file = ev.dataTransfer.items[i].getAsFile();
+          console.log('... file[' + i + '].name = ' + file.name);
+        }
       }
     }
   } else {
     // Use DataTransfer interface to access the file(s)
-    for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-      console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+    if (ev.dataTransfer.files.length > 1) {
+      alert("Cannot load multiple files.")
+    } else {
+      for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+       console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+      }
     }
   }
 }
